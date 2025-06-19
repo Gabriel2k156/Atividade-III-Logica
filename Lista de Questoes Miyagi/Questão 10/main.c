fe85ed1f0a8ea1875a6520b19ab375088f59c451
Gabriel Marques <gabrielmarx04@gmail.com>
2025-06-19 15:37:44 -0300

    Add files via upload

---
+++ Lista de Questoes Miyagi/Questão 10/main.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+int main(){
+	
+	int a, b, resultado;
+	char tipodeoperacao; // Cria uma string chamada "tipodeoperacao" //
+	tipodeoperacao = '+'; // Armazena o caracter "+" na string //
+	tipodeoperacao = '-'; // Armazena o caracter "-" na string //
+	tipodeoperacao = '*'; // Armazena o caracter "*" na string //
+	tipodeoperacao = '/'; // Armazena o caracter "/" na string //
+	
+	printf("Qual operacao deseja fazer?\n");
+	printf("+ para adicao\n- para subtracao\n* para multiplicacao\n/ para divisao\n\n");
+	scanf("%c", &tipodeoperacao); // Capta a entrada do usuario e associa a string "tipodeoperacao"
+	
+	switch(tipodeoperacao){
+		case '+':	
+		printf("Digite dois valores\n");
+		scanf("%d %d", &a, &b);	
+		resultado = adicao(a, b);
+		printf("O resultado da adicao e %d", resultado);
+		break;
+		
+		case '-':			
+		printf("Digite dois valores\n");
+		scanf("%d %d", &a, &b);	
+		resultado = subtracao(a, b);
+		printf("O resultado da subtracao e %d", resultado);
+		break;
+		
+		case '*':			
+		printf("Digite dois valores\n");
+		scanf("%d %d", &a, &b);	
+		resultado = multiplicacao(a, b);
+		printf("O resultado da multiplicacao e %d", resultado);
+		break;
+		
+		case '/':			
+		printf("Digite dois valores\n");
+		scanf("%d %d", &a, &b);	
+		resultado = divisao(a, b);
+		printf("O resultado da divisao e %d", resultado);
+		break;	
+	}
+	
+	return 0;
+}
---
+++ Lista de Questoes Miyagi/Questão 10/questaodez.c
@@ -0,0 +1,15 @@
+int adicao(int a, int b){
+	return a + b;
+}
+
+int subtracao(int a, int b){
+	return a - b;
+}
+
+int multiplicacao(int a, int b){
+	return a * b;
+}
+
+int divisao(int a, int b){
+	return a / b;
+}
