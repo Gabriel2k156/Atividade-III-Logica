4122a9ed05df8c0c904a4f711ccb6477b6f1f3d8
Gabriel Marques <gabrielmarx04@gmail.com>
2025-06-20 20:25:34 -0300

    Update main.c

--- Lista de Questoes Miyagi/Questao 8/main.c
+++ Lista de Questoes Miyagi/Questao 8/main.c
@@ -1,13 +1,13 @@
 include <stdio.h>
 int main(){
-    int base, expoente, resultado;
+    int base, expoente, resultado; // Declaração das variáveis utilizadas //
 
     printf("digite a base e o expoente\n");
-    scanf("%d %d", &base, &expoente);
+    scanf("%d %d", &base, &expoente); // Registra as entradas do usuário e os associa às variáveis "base" e "expoente"
 
-    resultado = potencia(base, expoente);//chama a função potencia
-
-    printf("%d elevado a %d = %d\n", base, expoente, resultado);
+    resultado = potencia(base, expoente); // Chama a função "potencia" para calcular o resultado // 
+    
+    printf("%d elevado a %d = %d\n", base, expoente, resultado); // Informa o resultado ao usuário //
 
     return 0;
 }
