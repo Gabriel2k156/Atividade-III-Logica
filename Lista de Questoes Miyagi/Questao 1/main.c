4630fba7d4d3f9a2407a87130d417d75d497a549
Gabriel Marques <gabrielmarx04@gmail.com>
2025-06-20 19:55:17 -0300

    Update main.c

--- Lista de Questoes Miyagi/Questao 1/main.c
+++ Lista de Questoes Miyagi/Questao 1/main.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 int main(){
-int a, b, resultado	;
+int a, b, resultado	; // Declaração de variávels //
 
 printf("Digite dois numeros\n");
-scanf("%d %d", &a, &b);
+scanf("%d %d", &a, &b); // Solicita entradas do usuário //
 
-resultado = soma(a, b);
+resultado = soma(a, b); // Chama a função "soma" //
 
-printf("O resultado da soma e %d", resultado);
+printf("O resultado da soma e %d", resultado); // Informa o resultado da soma ao usuário //
 
 return 0;
 
