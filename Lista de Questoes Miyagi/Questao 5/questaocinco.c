87c261706bd007dd00c6770691d96f073808c1c6
Gabriel Marques <gabrielmarx04@gmail.com>
2025-06-20 20:13:20 -0300

    Update questaocinco.c

--- Lista de Questoes Miyagi/Questao 5/questaocinco.c
+++ Lista de Questoes Miyagi/Questao 5/questaocinco.c
@@ -1,4 +1,4 @@
-float celsiusParaFahrenheit(float C, float F){	
+float celsiusParaFahrenheit(float C, float F){ // Criação de função chamada celsiusParaFahrenheit //
 
-	return (C * 1.8 + 32);
+	return (C * 1.8 + 32); // Retorna o resultado do cálculo de conversão C >>> F //
 }
